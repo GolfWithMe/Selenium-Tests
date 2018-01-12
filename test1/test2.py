44df4d3a1808a732bff233c8213ef896584023b6
Mike <netnutmike@gmail.com>
2018-01-12 12:08:33 -0800

    expanding the first test

--- test1/test2.py
+++ test1/test2.py
@@ -1,6 +1,8 @@
 import unittest
 from selenium import webdriver
-#from selenium.webdriver.common.keys import keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 class TestGroupActions(unittest.TestCase):
 
@@ -20,6 +22,28 @@ class TestGroupActions(unittest.TestCase):
         loginButton = driver.find_element_by_name("Submit")
 
         loginName.send_keys("mikie")
+        loginPassword.send_keys("1qaz2wsx#EDC")
+        loginButton.click()
+
+        driver.get("http://dev.golfwithme.biz/index.php?option=com_community&view=groupschedule&task=newrequest&Itemid=132")
+
+        groupName = Select(driver.find_element_by_name("selgroup"))
+        groupName.select_by_visible_text("Beaver Creek Country Club")
+
+        groupNameField = driver.find_element_by_name("selgroup")
+
+        groupNameField.submit()
+
+        selectList = Select(driver.find_element_by_name("selmember[]"))
+        selectList.select_by_visible_text("Mike Myers")
+        selectList.select_by_visible_text("test me")
+        selectList.select_by_visible_text("mikie3")
+
+        selectListField = driver.find_element_by_id("selmember")
+
+        #selectListField.submit()
+
+
 
 
 if __name__ == '__main__':
